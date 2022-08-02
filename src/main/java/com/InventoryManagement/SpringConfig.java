f48bc8fe9fc378bdd5e0e6a38ab503df4830939e
Lykasha <lykashalysongtseng@gmail.com>
2022-08-02 15:53:13 -0500

    Added @PropertySource in SpringConfig

--- src/main/java/com/InventoryManagement/SpringConfig.java
+++ src/main/java/com/InventoryManagement/SpringConfig.java
@@ -1,28 +1,19 @@
 package com.InventoryManagement;
 
-import java.sql.SQLException;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.AbstractApplicationContext;
-
-import com.InventoryManagement.controllers.ItemController;
-import com.InventoryManagement.data.ItemRepo;
 
 
 //@Import({DatabaseLayerConfig.class}) //if we wanted to load stuff manually with a ton of annotations
 //the main config file that will import other config files
 //all of our spring configuration is done here
 //the controller is decoupled from the dao and is unaware of its creation thanks to this config file through spring
-//@PropertySource("classpath:application.properties") // load these properties into the Environment
+//@PropertySource("classpath:application.properties") 
 @Configuration
 @ComponentScan(basePackages = {"com.InventoryManagement"})
+@PropertySource("classpath:application-${spring.profiles.active}.properties")// load these properties into the Environment
 public class SpringConfig {
 	public static void main(String[] args) {
 		//start the spring app by creating the IOC container
