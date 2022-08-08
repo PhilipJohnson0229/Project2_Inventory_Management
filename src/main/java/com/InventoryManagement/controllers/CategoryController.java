c0f76b8c0e78bb97bd1435efc18cf449b3c84158
Lykasha <lykashalysongtseng@gmail.com>
2022-08-08 13:50:07 -0500

    Added CategoryRepo

--- src/main/java/com/InventoryManagement/controllers/CategoryController.java
+++ src/main/java/com/InventoryManagement/controllers/CategoryController.java
@@ -1,5 +1,34 @@
 package com.InventoryManagement.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.InventoryManagement.beans.Category;
+import com.InventoryManagement.data.CategoryRepo;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
+    @Autowired
+    private CategoryRepo repo;
+
+    
+    @GetMapping("/getCategories")
+    public List<Category> getCategories(){
+        return repo.findAll();
+    }
+    
+    @GetMapping("/category/{id}")
+    public Optional<Category> getTestCategory(@PathVariable int id){
+        return repo.findById(id);
+    }
+    
+    
 }
