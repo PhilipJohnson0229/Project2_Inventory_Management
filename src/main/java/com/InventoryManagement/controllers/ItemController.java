70d4109e125c63af01795922ae1f9a06c585ab2c
Lykasha <lykashalysongtseng@gmail.com>
2022-08-08 13:46:24 -0500

    Updated ItemService and ItemController

--- src/main/java/com/InventoryManagement/controllers/ItemController.java
+++ src/main/java/com/InventoryManagement/controllers/ItemController.java
@@ -5,117 +5,61 @@ import java.util.List;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.InventoryManagement.beans.Item;
-import com.InventoryManagement.data.ItemRepo;
 import com.InventoryManagement.service.ItemService;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-
-
+//receives requests and passes the params and path vriables to the service
 @RestController
-@RequestMapping("/item/")
-@CrossOrigin(origins = "*") // Spring MVC doesn't allow CORS by default
-@Tag(description = "Artist API", name = "API Info")
+@CrossOrigin(origins = "*")
 public class ItemController {
 	
-	@Autowired
-	private ItemRepo repo;
+	private static final Logger log = LoggerFactory.getLogger(ItemController.class);
 	
-	//private static final Logger log = LoggerFactory.getLogger(ItemController.class);
+    @Autowired
+    private ItemService service;
 
-	//means this method will be mapped do a GET request
-	//@ResponseBody //this tells spring not to redirect to another page just inject data into response body
-	/*@GetMapping("{id}")
-	public Item findById(@PathVariable int id) 
-	{
-		return repo.findById(id).get();
-	}*/
-	
-	
-	
-	@PostMapping("/add")
-	public void addItemToDB(@RequestParam int id, @Valid @RequestParam String name, @RequestParam int catId, @RequestParam int strId){
-		 Item newItem = new Item();
-		 newItem.setId(id);
-		 newItem.setName(name);
-		 
-	     repo.save(newItem);
-	}
-	
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Item> findById(@PathVariable int id, Authentication principal) {
-		// SecurityContext - stores all Authentication info in HttpSession
-		//SecurityContextHolder
-		//log.debug("we're in the findById method"); // INFO default threshold
-		//log.debug("findById URL: /artist/" + id);
-		//log.info("Current user: " + principal.getName());
-		return ResponseEntity.ok(repo.findById(id).orElse(new Item()));
-	}
-	
-	@PostMapping 
-	@Transactional  
-	public ResponseEntity<Item> save(@Valid @RequestBody Item artist) { // 400
-		return new ResponseEntity<>(repo.save(artist), HttpStatus.CREATED);
+    @GetMapping("/getItems")
+	@ResponseBody					//Pagination -> ItemRepo extends PagingAndSortingRepository
+    public List<Item> getItems(@RequestParam(defaultValue = "0") int page){
+        return service.findAll(page);
+    }
+    
+    @GetMapping("/getStoreItemsId")
+    public List<Item> getItemsByStoreId(@RequestParam(name = "id", required = true) int id){
+    	log.info("using the getItemsByStoreId method and the requested id is " + id);
+        return service.findItemByStoreId(id);
+    }
+    
+    @GetMapping("/getStoreItemsName")
+    public List<Item> getItemsByStoreName(@RequestParam(name = "name", required = true) String name){
+        return service.findItemByStoreName(name);
+    }
+    
+    @PostMapping // objectMapper.readValue(req.getInputStream(), Artist.class)
+	@Transactional // 
+	public ResponseEntity<Item> save(@Valid @RequestBody Item item) { // 400
+		return new ResponseEntity<>(service.save(item), HttpStatus.CREATED);
 	}
 	
 	@PostMapping("/many")  
-	@Transactional 
+	@Transactional //(rollbackFor = org.h2.jdbc.JdbcSQLDataException.class)
 	public ResponseEntity<Void> saveMany(@RequestBody List<Item> items) { 
-		for (Item artist : items) {
-			repo.save(artist); // any one fails - rollback
+		for (Item item : items) {
+			service.save(item); // any one fails - rollback
 		}
 		return ResponseEntity.noContent().build();
-	}
-	 
-	@GetMapping
-	@ResponseBody
-	public Object findAll(@RequestParam (required = false) String name) 
-	{
-		if(name != null) 
-		{
-			return repo.findByNameLike("%" + name + "%");
-		}else 
-		{
-			return repo.findAllItemsComplete();
-		} 
-	}
-	
-	@DeleteMapping
-	public ResponseEntity<Void> delete(@PathVariable int id) 
-	{
-		repo.deleteById(id);
-		return ResponseEntity.noContent().build();
-	}
-	
-	@PutMapping("{id}") // PUT 
-	public Item update(@RequestBody Item item, @PathVariable int id) {
-		if (repo.existsById(id)) {
-			item.setId(id); // don't trust user to use your system as intended
-			return repo.save(item);
-		} else {
-			// you ain't updating, you saving
-			throw new IllegalArgumentException("ID doesn't exist");
-		}
 	}
 }
--- src/main/java/com/InventoryManagement/service/ItemService.java
+++ src/main/java/com/InventoryManagement/service/ItemService.java
@@ -1,18 +1,57 @@
 package com.InventoryManagement.service;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.InventoryManagement.beans.Item;
+import com.InventoryManagement.data.ItemRepo;
 
 
+//provides layer of abstraction between controller and repo
+
+//controllers will take in request information
+//services will house business logic 
 
 @Service
 public class ItemService {
-
-	public ItemService() {
-		System.out.println("new ItemService()");
-	}	
 	
+	private static final int DEFAULT_PAGE_SIZE = 10;
+	
+	@Autowired
+	private ItemRepo repo;
+	
+	
+	public List<Item> findItemByStoreId(int storeId)
+	{
+		return repo.findByStoreId(storeId);
+	} 
+	
+	
+	public List<Item> findItemByStoreName(String name)
+	{
+		return repo.findByStoreName(name);
+	} 
+	
+	
+	//delegation pattern - provides a layer between two components for more control
+	public Long count() 
+	{
+		return repo.count();
+	}
+	
+	
+	public List<Item> findAll(int page) 
+	{
+		return repo.findAll(PageRequest.of(page, DEFAULT_PAGE_SIZE)).toList();
+	}
+	
+	
+	public Item save(Item item) 
+	{
+		return repo.save(item);
+	}
 }
