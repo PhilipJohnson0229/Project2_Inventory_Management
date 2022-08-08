325b97e3984b14fd1e8169ee746ca68dd9127f15
Lykasha <lykashalysongtseng@gmail.com>
2022-08-08 13:47:37 -0500

    Updated ItemRepo

--- src/main/java/com/InventoryManagement/data/ItemRepo.java
+++ src/main/java/com/InventoryManagement/data/ItemRepo.java
@@ -2,21 +2,25 @@ package com.InventoryManagement.data;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.InventoryManagement.beans.Item;
 
 @Repository						  //change this to JpaRepository when ready to go past basic CRUD
-public interface ItemRepo extends JpaRepository<Item, Integer>{
+public interface ItemRepo extends PagingAndSortingRepository<Item, Integer>{
 	
-	// a find by/query method
-	@Query( nativeQuery = true, value = "select i.name, i.price, c.name, c.description, s.name from item i join Store s on i.strFk = s.id join Category c on i.catFk = c.id") 
-	public List <Item> findAllItemsComplete();
+	//@Query("from Item i inner join i.store str inner join i.category cat") 
+	//this is if we NEED to reduce databse hits
+	public Page<Item> findAll(Pageable request);
 	
-	public List <Item> findByNameLike(String name);
-	//even though were trying to retrieve a single item we have to do this to return a UNIQUE result
-	//if more than one item is found then it will return an iterable hqlQuery.List()
+	@Query("From Item i inner join i.store str where str.name = ?1")
+	public List<Item> findByStoreName(String name);
+	
+	@Query("From Item i inner join i.store str where str.id = ?1")
+	public List<Item> findByStoreId(int storeId);
 }
